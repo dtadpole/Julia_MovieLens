d180f8e16446cb4eefb4b3670da185c133e64875
D.Tadpole <gsk390@gmail.com>
2022-09-26 03:30:21 -0700

    loss

--- bert4rec.jl
+++ bert4rec.jl
@@ -145,7 +145,8 @@ lossF = (model, x, masks) -> begin
     y_truth = reshape(x .* masks, (1, size(x)...)) .== 1:MOVIE_SIZE     # (VOCAB_SIZE, SEQ_LEN, BATCH_SIZE)
     y_truth = Float32.(y_truth)
 
-    loss_embed_sum = reshape(-sum(y_truth .* log.(y), dims=(1, 2)), :)  # (BATCH_SIZE,)
+    loss_embed = y_truth .* log.(y)
+    loss_embed_sum = reshape(-sum(loss_embed, dims=(1, 2)), :)          # (BATCH_SIZE,)
 
     masks_sum = reshape(1 ./ sum(masks, dims=1), :)                     # (SEQ_LEN, BATCH_SIZE) => (BATCH_SIZE)
 
